3d561446945b49b0c37806440bcbfe73b12c26a2
Yasin Cetindil <ycetindil@gmail.com>
2023-08-24 10:53:19 -0700

    Finish storage_container

--- modules/azure/storage_container/main.tf
+++ modules/azure/storage_container/main.tf
@@ -1,5 +1,8 @@
+# Manages a Container within an Azure Storage Account.
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_container
 resource "azurerm_storage_container" "storage_container" {
   name                  = var.name
   storage_account_name  = var.storage_account_name
   container_access_type = var.container_access_type
+  metadata              = var.metadata
 }
---
+++ modules/azure/storage_container/variables.tf
@@ -0,0 +1,34 @@
+variable "name" {
+  description = <<EOD
+		 - (Required) The name of the Container which should be created within the Storage Account.
+		Changing this forces a new resource to be created.
+	EOD
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = <<EOD
+		 - (Required) The name of the Storage Account where the Container should be created.
+		Changing this forces a new resource to be created.
+	EOD
+  type        = string
+}
+
+variable "container_access_type" {
+  description = <<EOD
+		 - (Optional) The Access Level configured for this Container.
+		Possible values are blob, container or private.
+		Defaults to private.
+	EOD
+  default     = null
+  type        = string
+}
+
+variable "metadata" {
+  description = <<EOD
+		 - (Optional) A mapping of MetaData for this Container.
+		 All metadata keys should be lowercase.
+	EOD
+  default     = null
+  type        = map(string)
+}
