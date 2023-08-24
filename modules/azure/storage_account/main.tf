a6c3c71f9a4fe2c7b6186f5d852ab904c71d2d46
Yasin Cetindil <ycetindil@gmail.com>
2023-08-24 09:24:43 -0700

    Finish storage_account

--- modules/azure/storage_account/main.tf
+++ modules/azure/storage_account/main.tf
@@ -1,3 +1,5 @@
+# Manages an Azure Storage Account.
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 resource "azurerm_storage_account" "storage_account" {
   name                     = var.name
   resource_group_name      = var.resource_group_name
@@ -5,12 +7,4 @@ resource "azurerm_storage_account" "storage_account" {
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
   tags                     = var.tags
-}
-
-resource "azurerm_storage_container" "storage_containers" {
-  for_each = var.containers
-
-  name                  = var.name
-  storage_account_name  = azurerm_storage_account.storage_account.name
-  container_access_type = var.container_access_type
 }
--- modules/azure/storage_account/variables.tf
+++ modules/azure/storage_account/variables.tf
@@ -1,32 +1,51 @@
 variable "name" {
-  type = string
+  description = <<EOD
+		(Required) Specifies the name of the storage account. Only lowercase Alphanumeric characters allowed.
+		Changing this forces a new resource to be created.
+		This must be unique across the entire Azure service, not just within the resource group.
+	EOD
+  type        = string
 }
 
 variable "resource_group_name" {
-  type = string
+  description = <<EOD
+		 - (Required) The name of the resource group in which to create the storage account.
+		Changing this forces a new resource to be created.
+	EOD
+  type        = string
 }
 
 variable "location" {
-  type = string
+  description = <<EOD
+		 - (Required) Specifies the supported Azure location where the resource exists.
+		Changing this forces a new resource to be created.
+	EOD
+  type        = string
 }
 
 variable "account_tier" {
-  type = string
+  description = <<EOD
+		 - (Required) Defines the Tier to use for this storage account.
+		Valid options are Standard and Premium.
+		For BlockBlobStorage and FileStorage accounts only Premium is valid.
+		Changing this forces a new resource to be created.
+		NOTE: Blobs with a tier of Premium are of account kind StorageV2.
+	EOD
+  type        = string
 }
 
 variable "account_replication_type" {
-  type = string
+  description = <<EOD
+		 - (Required) Defines the type of replication to use for this storage account.
+		Valid options are LRS, GRS, RAGRS, ZRS, GZRS and RAGZRS.
+	EOD
+  type        = string
 }
 
 variable "tags" {
-  default = null
-  type    = map(string)
-}
-
-variable "containers" {
-  default = {}
-  type = map(object({
-    name                  = string
-    container_access_type = string
-  }))
+  description = <<EOD
+		(Optional) A mapping of tags to assign to the resource.
+	EOD
+  default     = null
+  type        = map(string)
 }
