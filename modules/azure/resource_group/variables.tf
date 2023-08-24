7a4da3656f6f4919838d0780fe9a75c58c2aedfa
Yasin Cetindil <ycetindil@gmail.com>
2023-08-24 08:44:25 -0700

    Finish resource_group

--- modules/azure/resource_group/variables.tf
+++ modules/azure/resource_group/variables.tf
@@ -1,7 +1,31 @@
-variable "name" {
-  type = string
+variable "location" {
+  description = <<EOD
+		(Required) The Azure Region where the Resource Group should exist.
+		Changing this forces a new Resource Group to be created.
+	EOD
+  type        = string
 }
 
-variable "location" {
-  type = string
+variable "name" {
+  description = <<EOD
+		(Required) The Name which should be used for this Resource Group.
+		Changing this forces a new Resource Group to be created.
+	EOD
+  type        = string
+}
+
+variable "managed_by" {
+  description = <<EOD
+		(Optional) The ID of the resource or application that manages this Resource Group.
+	EOD
+  default     = null
+  type        = string
+}
+
+variable "tags" {
+  description = <<EOD
+		(Optional) A mapping of tags which should be assigned to the Resource Group.
+	EOD
+  default     = null
+  type        = map(string)
 }
