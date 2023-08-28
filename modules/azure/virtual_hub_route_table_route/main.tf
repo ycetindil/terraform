44b51d1217ae40194adc0aee682ea36a27505ca0
Yasin Cetindil <ycetindil@gmail.com>
2023-08-28 09:20:58 -0700

    Finish virtual_hub_route_table_route

--- modules/azure/virtual_hub_route_table_route/main.tf
+++ modules/azure/virtual_hub_route_table_route/main.tf
@@ -1,16 +1,11 @@
+# Manages a Route in a Virtual Hub Route Table.
+# Note: Route table routes can managed with this resource, or in-line with the virtual_hub_route_table resource. Using both is not supported.
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_hub_route_table_route
 resource "azurerm_virtual_hub_route_table_route" "virtual_hub_route_table_route" {
-  for_each = local.route_table_routes
-
-  route_table_id = azurerm_virtual_hub.virtual_hubs[var.virtual_hub].default_route_table_id
-
+  route_table_id    = var.route_table_id
   name              = var.name
-  destinations_type = var.destinations_type
   destinations      = var.destinations
+  destinations_type = var.destinations_type
   next_hop_type     = var.next_hop_type
-  # We need the connection's reference name rather than its name, hence the 'replace' function
-  next_hop = (
-    var.next_hop.firewall != {} ?
-    data.azurerm_firewall.route_table_route_firewalls[each.key].id :
-    azurerm_virtual_hub_connection.virtual_hub_connections["${var.virtual_hub}_${replace(var.next_hop.connection_name, "-", "_")}"].id
-  )
+  next_hop          = var.next_hop
 }
---
+++ modules/azure/virtual_hub_route_table_route/variables.tf
@@ -0,0 +1,47 @@
+variable "route_table_id" {
+  description = <<EOD
+		 - (Required) The ID of the Virtual Hub Route Table to link this route to.
+		Changing this forces a new resource to be created.
+	EOD
+  type        = string
+}
+
+variable "name" {
+  description = <<EOD
+		 - (Required) The name which should be used for this route.
+		Changing this forces a new resource to be created.
+	EOD
+  type        = string
+}
+
+variable "destinations" {
+  description = <<EOD
+		 - (Required) A list of destination addresses for this route.
+	EOD
+  type        = set(string)
+}
+
+variable "destinations_type" {
+  description = <<EOD
+		 - (Required) The type of destinations.
+		Possible values are CIDR, ResourceId and Service.
+	EOD
+  type        = string
+}
+
+variable "next_hop" {
+  description = <<EOD
+		 - (Required) The next hop's resource ID.
+	EOD
+  type        = string
+}
+
+variable "next_hop_type" {
+  description = <<EOD
+		 - (Optional) The type of next hop.
+		Currently the only possible value is ResourceId.
+		Defaults to ResourceId.
+	EOD
+  default     = null
+  type        = string
+}
