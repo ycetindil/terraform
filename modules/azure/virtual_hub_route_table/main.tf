885b5c28e64fd3ab27a2d99a0478150d2ccbb678
Yasin Cetindil <ycetindil@gmail.com>
2023-08-28 09:15:11 -0700

    Finish virtual_hub_route_table

--- modules/azure/virtual_hub_route_table/main.tf
+++ modules/azure/virtual_hub_route_table/main.tf
@@ -1,7 +1,8 @@
+# Manages a Virtual Hub Route Table.
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_hub_route_table
 # CYCLE ALERT! Letting the route tables create their own routes causes a cycle because the routes can refer to the connections and connections already refer to the route tables. Hence the 'rooute_table_routes' are created in the next block along with the 'Default table' routes.
 resource "azurerm_virtual_hub_route_table" "virtual_hub_route_table" {
-  for_each = local.route_tables
-
   name           = var.name
-  virtual_hub_id = azurerm_virtual_hub.virtual_hubs[var.virtual_hub].id
+  virtual_hub_id = var.virtual_hub_id
+  labels         = var.labels
 }
---
+++ modules/azure/virtual_hub_route_table/variables.tf
@@ -0,0 +1,25 @@
+variable "name" {
+  description = <<EOD
+		 - (Required) The name which should be used for Virtual Hub Route Table.
+		Changing this forces a new resource to be created.
+	EOD
+  type        = string
+}
+
+variable "virtual_hub_id" {
+  description = <<EOD
+		 - (Required) The ID of the Virtual Hub within which this route table should be created.
+		Changing this forces a new resource to be created.
+	EOD
+  type        = string
+}
+
+variable "labels" {
+  description = <<EOD
+		 - (Optional) List of labels associated with this route table.
+	EOD
+  default     = null
+  type        = set(string)
+}
+
+# routes are omitted since they are created with virtual_hub_route_table_route resource.
