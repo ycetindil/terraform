c037f0f9b98c1b70af4e29ac0c4d8e9658d92668
Yasin Cetindil <ycetindil@gmail.com>
2023-08-28 09:29:39 -0700

    Finish virtual_wan

--- modules/azure/virtual_wan/main.tf
+++ modules/azure/virtual_wan/main.tf
@@ -1,4 +1,5 @@
-#
+# Manages a Virtual WAN.
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_wan
 resource "azurerm_virtual_wan" "virtual_wan" {
   name                = var.name
   resource_group_name = var.resource_group_name
--- modules/azure/virtual_wan/variables.tf
+++ modules/azure/virtual_wan/variables.tf
@@ -1,64 +1,23 @@
 variable "name" {
-  type = string
+  description = <<EOD
+		 - (Required) Specifies the name of the Virtual WAN.
+		Changing this forces a new resource to be created.
+	EOD
+  type        = string
 }
 
 variable "resource_group_name" {
-  type = string
+  description = <<EOD
+		 - (Required) The name of the resource group in which to create the Virtual WAN.
+		 Changing this forces a new resource to be created.
+	EOD
+  type        = string
 }
 
 variable "location" {
-  type = string
-}
-
-variable "virtual_hubs" {
-  default = {}
-  type = map(object({
-    name           = string
-    address_prefix = string
-    firewall = optional(object({
-      existing = optional(object({
-        name                = string
-        resource_group_name = string
-      }), null)
-      new = optional(any, null) // Pass directly to 'firewall' module
-      firewall_policy = optional(object({
-        existing = optional(object({
-          name                = string
-          resource_group_name = string
-        }), null)
-        new = optional(any, null) // Pass directly to 'firewall_policy' module
-      }), null)
-    }), null)
-    virtual_hub_connections = optional(map(object({
-      name = string
-      remote_virtual_network = object({
-        name                = string
-        resource_group_name = string
-      })
-      routing = object({
-        associated_route_table = string
-        propagated_route_tables = list(object({
-          name        = string
-          virtual_hub = string
-        }))
-      })
-    })), {})
-    route_tables = optional(map(object({
-      name = string
-    })), {})
-    route_table_routes = optional(map(object({
-      name              = string
-      route_table       = string
-      destinations_type = string
-      destinations      = list(string)
-      next_hop_type     = string
-      next_hop = object({
-        firewall = optional(object({
-          name                = string
-          resource_group_name = string
-        }))
-        connection_name = optional(string, "")
-      })
-    })), {})
-  }))
+  description = <<EOD
+		 - (Required) Specifies the supported Azure location where the resource exists.
+		Changing this forces a new resource to be created.
+	EOD
+  type        = string
 }
